dc9b8ec9bcbd5d09e137ff3051e336b3b1102228
Nolan Perry <nperry@bridgespreparatoryschool.org>
2019-03-11 01:46:29 -0400

    Add Base Functions
    It Gets up to the Point where it says "Yes, The password and username is correct"

---
+++ handler.php
@@ -0,0 +1,122 @@
+<?php
+// Report all errors
+error_reporting(E_ALL);
+require"config.php";
+
+$return = $_POST["return"];
+$last_name = $_POST["last"];
+$first_name = $_POST["first"];
+$password = $_POST["password"];
+$remember = $_POST["remember"];
+
+/**
+  * Returns a OpenSim UUID based on the Provided First and Last Name
+  * This Queries your Database to get the UUID of the User Logging In
+  * @param string $first The First Name of the User
+  * @param string $last The Last Name of the User
+  * @param object $db The Database Object Connected to the ROBUST DB
+  * @return string $uuid The UUID of your person
+  */
+function get_uuid_by_user($first, $last, $db){
+
+$sql = "SELECT `PrincipalID` FROM `useraccounts` WHERE `FirstName` = '" . $first ."' AND `LastName` = '" . $last . "' LIMIT 1";
+$result = mysqli_query($db, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $uuid = $row["PrincipalID"];
+    }
+} else {
+   die();
+}
+return $uuid;
+}
+
+/**
+  * Retrieves the Hash from the ROBUST AUTH Table
+  * @param string $uuid The UUID of your User
+  * @param object $db DB Object of your ROBUST Instance
+  * @return string $hash The UUID of your person
+  */
+
+function get_hash_by_uuid($uuid, $db){
+$sql = "SELECT `passwordHash` FROM `auth` WHERE `UUID` = '" . $uuid . "' LIMIT 1";
+$result = mysqli_query($db, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $hash = $row["passwordHash"];
+    }
+} else {
+   die();
+}
+return $hash;
+}
+
+/**
+  * Retrieves the Salt from the ROBUST AUTH Table
+  * @param string $uuid The UUID of your User
+  * @param object $db DB Object of your ROBUST Instance
+  * @return string $salt The SALT of your person
+  */
+function get_salt_by_uuid($uuid, $db){
+$sql = "SELECT `passwordSalt` FROM `auth` WHERE `UUID` = '" . $uuid . "' LIMIT 1";
+$result = mysqli_query($db, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $salt = $row["passwordSalt"];
+    }
+} else {
+   die();
+}
+return $salt;
+}
+
+
+/**
+  * Attemps to See if your so called person is correct in their auth
+  * @param string $salt The Salt
+  * @param string $hash Hash
+  * @param string $pass the password submitted
+  * @return bool $correct TRUE/False if all documents are in order
+  */
+function check_my_id($salt, $hash, $pass){
+$p = md5($pass);
+$attempt = md5($p.":".$salt);
+
+//$a2 = md5($attempt);
+
+
+
+
+
+if($attempt == $hash)
+{
+return TRUE;
+}
+else{
+return FALSE;
+}
+
+
+
+}
+
+
+/// get to work
+
+$uuid = get_uuid_by_user($first_name, $last_name, $conn);
+
+
+// echo "Hash : " . get_hash_by_uuid($uuid, $conn) . "  |  Salt : " . get_salt_by_uuid($uuid, $conn);
+
+$r_hash = get_hash_by_uuid($uuid, $conn);
+$r_salt = get_salt_by_uuid($uuid, $conn);
+
+$validity =  check_my_id($r_salt, $r_hash, $password);
+
+
+echo $validity;
+
+
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+require"config.php";
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Neverworld Grid Login</title>
+  <link rel="stylesheet" href="style.css">
+  <script type="text/javascript">
+    function submitForm(myfield,e) {
+        var keycode; if (window.event) keycode = window.event.keyCode; else if (e) keycode = e.which; else return true; if (keycode == 13) { myfield.form.submit(); return false; } else return true;
+    }
+  </script>
+</head>
+<body>
+  <div class="page-wrap">
+    <div class="content">
+        <a href="https://www.neverworldgrid.com"><img class="logo" src="logo.jpg" alt="Neverworld Logo"></a>
+        
+        
+          <form method="POST" action="handler.php">
+            <input name="return" value="<?php echo $_REQUEST['return'];?>" type="hidden"/>
+            <div class="row">
+              <label for="first" class="visuallyHidden"></label>
+              <input type="text" id="first" placeholder="Firstname" name="first" autocomplete="yes"/>
+            </div>
+<div class="row">
+              <label for="last" class="visuallyHidden"></label>
+              <input type="text" id="last" placeholder="Last Name" name="last" autocomplete="yes"/>
+            </div>
+
+            <div class="row">
+              <label for="password" class="visuallHidden"></label>
+              <input type="password" id="password" placeholder="Password" name="password" autocomplete="yes" onKeyPress="return submitForm(this, event;" />
+            </div>
+            <div class="row">
+              <input type="checkbox" id="remember" class="checkbox" name="remember_me">
+              <label for="remember" class="label">Remember me</label>
+              <a href="https://secondlife.com/my/account/request.php" class="forgot">Forgot your login?</a>
+            </div>
+            <div class="row last">
+              <button type="submit" class="login" id="login">LOGIN</button>
+              <a href="join" class="join" id="join"><span>JOIN NOW</span></a>
+            </div>
+          </form>
+        
+    </div>
+  </div>
+  <footer class="footer">
+    <ul class="footer_links">
+      <li>
+        <a target="_blank" href="http://www.neverworldgrid.com">Neverworld Grid</a>
+      </li>
+         <li>
+        <span class="copyright">&copy; 2019 Neverworld Grid, All Rights Reserved</a></span>
+      </li>
+    </ul>
+  </footer>
+</body>
+</html>
