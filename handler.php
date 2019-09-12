3c7f1db73038da63017a6596687eaa13484e2941
Nolan Perry <nolan.perry.sc@gmail.com>
2019-09-12 12:50:54 -0400

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,7 +1,7 @@
 <?php
 /*
  OpenSimulator Web Login
-    Copyright (C) 2019 Neverworld Grid and its Contributors
+    Copyright (C) 2019 VortexSystems and its Contributors
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
